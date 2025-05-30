b8144071c5c023b44d90c2b8bf59be23345cd7ab
dgashuk <dmitrij.gashuk@gmail.com>
2025-05-30 22:18:59 +0300

    new string provider

--- src/main/java/com/epam/dmytro_hashuk/Main.java
+++ src/main/java/com/epam/dmytro_hashuk/Main.java
@@ -1,10 +1,7 @@
 package com.epam.dmytro_hashuk;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String name = "Camile";
-        System.out.printf("Hello and welcome %s!", name);
+        new StringProvider().getGreetings();
     }
 }
--- src/main/java/com/epam/dmytro_hashuk/StringProvider.java
+++ src/main/java/com/epam/dmytro_hashuk/StringProvider.java
@@ -1,4 +1,11 @@
 package com.epam.dmytro_hashuk;
 
 public class StringProvider {
+    private final String greetings = "Hello ";
+    private final String name = "World";
+    private final String separator = "_";
+
+    public void getGreetings(){
+        System.out.println(String.format("%s_%s !", greetings, name));
+    }
 }
