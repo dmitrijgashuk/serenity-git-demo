b0af8a4186488136a57baeb4d831aa35d2ca3987
dgashuk <dmitrij.gashuk@gmail.com>
2025-05-30 22:26:02 +0300

    StringBulder provider

--- src/main/java/com/epam/dmytro_hashuk/Main.java
+++ src/main/java/com/epam/dmytro_hashuk/Main.java
@@ -1,6 +1,13 @@
 package com.epam.dmytro_hashuk;
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("This is new commit");
+        StringBuilder builder = new StringBuilder();
+        builder.append("Hello");
+        builder.append("_");
+        builder.append("World");
+        builder.append("!");
+
+        String result = builder.toString();
+        System.out.println(result);
     }
 }
