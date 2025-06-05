08ae5f5a3c1575f91cf756a2fd8127809a2199f2
dgashuk <dmitrij.gashuk@gmail.com>
2025-06-05 21:52:14 +0300

    fw start v 1.0

---
+++ src/test/java/com/epam/dmytro_hashuk/wikitest/BaseWikiTest.java
@@ -0,0 +1,11 @@
+package com.epam.dmytro_hashuk.wikitest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BaseWikiTest {
+    @Test
+    public void checkWiki(){
+        Assertions.assertTrue(true);
+    }
+}
