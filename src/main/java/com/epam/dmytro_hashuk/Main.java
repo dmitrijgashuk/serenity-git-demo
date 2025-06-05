81ae6e99665f0b92f23b0dc18049500a31578695
dgashuk <dmitrij.gashuk@gmail.com>
2025-06-05 21:34:33 +0300

    get started

--- src/main/java/com/epam/dmytro_hashuk/Main.java
+++ src/main/java/com/epam/dmytro_hashuk/Main.java
@@ -2,6 +2,6 @@ package com.epam.dmytro_hashuk;
 
 public class Main {
     public static void main(String[] args) {
-        new StringProvider().getGreetings();
+
     }
 }
---
+++ src/test/java/com/epam/dmytro_hashuk/WikiQualityAssurance.java
@@ -0,0 +1,4 @@
+package com.epam.dmytro_hashuk;
+
+public class WikiQualityAssurance {
+}
