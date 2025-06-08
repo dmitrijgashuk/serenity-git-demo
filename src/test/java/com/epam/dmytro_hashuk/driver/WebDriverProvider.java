b46dce6c3ec999a40c3d897a0bec11b2c7bd2dcb
dgashuk <dmitrij.gashuk@gmail.com>
2025-06-08 10:37:49 +0300

    add action, base selenium test for chrome wiki

--- src/test/java/com/epam/dmytro_hashuk/driver/WebDriverProvider.java
+++ src/test/java/com/epam/dmytro_hashuk/driver/WebDriverProvider.java
@@ -1,4 +1,12 @@
 package com.epam.dmytro_hashuk.driver;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebDriverProvider {
+    public static WebDriver getDriver(){
+        WebDriver driver = new ChromeDriver();
+        return driver;
+    }
 }
