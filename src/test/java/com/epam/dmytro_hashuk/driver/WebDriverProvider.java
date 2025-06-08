71e12cba94caf88b648781c56248fa1e98c847c2
dgashuk <dmitrij.gashuk@gmail.com>
2025-06-08 11:00:59 +0300

    add action install chrome complete

--- src/test/java/com/epam/dmytro_hashuk/driver/WebDriverProvider.java
+++ src/test/java/com/epam/dmytro_hashuk/driver/WebDriverProvider.java
@@ -3,10 +3,13 @@ package com.epam.dmytro_hashuk.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverProvider {
     public static WebDriver getDriver(){
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
         return driver;
     }
 }
--- src/test/java/com/epam/dmytro_hashuk/wikitest/BaseWikiTest.java
+++ src/test/java/com/epam/dmytro_hashuk/wikitest/BaseWikiTest.java
@@ -1,12 +1,14 @@
 package com.epam.dmytro_hashuk.wikitest;
 
 import com.epam.dmytro_hashuk.driver.WebDriverProvider;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -14,17 +16,28 @@ import java.time.Duration;
 
 public class BaseWikiTest {
     public String wikiUrl = "https://www.wikipedia.org/";
+    public WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp(){
+        webDriver = WebDriverProvider.getDriver();
+    }
     @Test
     public void checkUkrWiki(){
         WebDriver webDriver = WebDriverProvider.getDriver();
         webDriver.get(wikiUrl);
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
 
-        WebElement ukrLangElement = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#js-link-box-uk")));
+        WebElement ukrLangElement = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#js-link-box-en")));
         ukrLangElement.click();
 
         WebElement wikiLogo = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("img.mw-logo-wordmark")));
         String currentAttribute = wikiLogo.getAttribute("alt");
-        Assertions.assertEquals("Вікіпедія", currentAttribute);
+        Assertions.assertEquals("Wikipedia", currentAttribute);
+    }
+
+    @AfterEach
+    public void shutDown(){
+
     }
 }
